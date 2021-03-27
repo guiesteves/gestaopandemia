ffeb751ec1bf3812149fc482036c54ca7d0b9693
Guilherme de Souza Esteves <guilherme.esteves@serpro.gov.br>
2021-03-27 19:26:14 -0300

    alteração para definição  de porta pela aplicação

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace CVC19
 {
@@ -15,6 +16,7 @@ namespace CVC19
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
                 });
     }
 }
